b88c39205f49ff105061b1ad89e69d70f6c4da7b
Ramkalame <ramkalame14082001@gmail.com>
2024-11-17 15:58:24 +0530

    componets are created

--- TadobaSolution/src/app/app.component.ts
+++ TadobaSolution/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+     HeaderComponent,
+      HomeComponent,
+       FooterComponent,
+        TermsAndConditionComponent,
+         AboutUsComponent,
+          ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
